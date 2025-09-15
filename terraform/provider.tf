221aa47ab4df7ea249aefc42d604932821ddf4b7
madxboct <mdxboct@gmail.com>
2025-09-15 11:51:11 +0300

    nnn

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,3 +14,10 @@ terraform {
 provider "ansible" {
   # Configuration options
 }
+
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  #token      = var.AWS_TOKEN
+  region     = var.AWS_REGION
+}
--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,12 +1,37 @@
-provider "aws" {
-  region = "eu-west-2"
+# ADD PUBLIC KEY TO AWS. IT WILL BE USED DURING VM CREATION
+resource "aws_key_pair" "aws_key" {
+  key_name   = "aws_key"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
+# SETUP SECURITY GROUP - AWS FIREWALL
+resource "aws_security_group" "sg_node" {
+  name = "sg_k8s"
+  # Any incoming traffic is allowed
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Any outgoing traffic is allowed
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 INSTANCE FOR MASTER
 resource "aws_instance" "node1" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-0c93d46fa03982829"
+  ami           = var.AMIS["ubuntu"]
+  instance_type = var.INSTANCE_TYPE
+  # Add previously generated public key to VM
+  key_name = aws_key_pair.aws_key.key_name
+  # Add VM to security group
+  vpc_security_group_ids = [aws_security_group.sg_node.id]
   tags = {
-    Name = "node1"
+     Name = "node1"
   }
 }
